b4e8a06ed74be57f1ff45b1cdb176f6fd582ab2e
fareska <fares.xb@gmail.com>
2020-11-05 09:30:17 +0200

    JQuery exercises 1+2

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,6 +1,89 @@
 
-const header = $("h1")
-console.log(header)
+/////////////////////jQuery-Exercises////////////////////////////
+
+//////////////exercise 1 + 2
+$("#theButton").on("click", function(){
+    let val = $("#exercise1").val()
+    $("ul").append(`<li>${val}</li>`)
+    $("#exercise1").val("")
+    
+    $("li").on("click", function(){ 
+    $(this).remove()
+})
+})
+
+//////////////exercise 4
+
+(function($) {
+    $.fn.getAttributes = function() {
+        var attributes = {}; 
+
+        if( this.length ) {
+            $.each( this[0].attributes, function( index, attr ) {
+                attributes[ attr.name ] = attr.value;
+            } ); 
+        }
+
+        return attributes;
+    };
+console.log($('.item').getAttributes())
+
+
+})
+
+// for (let i =0; i<3; i++){
+//         console.log($(".item").getAttributes())
+
+// }
+
+
+// const items = $(".item") 
+// console.log(items)
+// for (let item of items) {
+    // let attr = []
+//     // attr.push(item)
+//     console.log(item.data().instock)
+
+// }
+
+
+// for (let item of items){
+    
+//     if (item){
+//         console.log(item)
+//     }
+    
+
+// }
+
+//console.log($(".item"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+/////////////////////jQuery-Material////////////////////////////
+// const header = $("h1")
+// console.log(header)
 
 // console.log(jQuery)
 // console.log($)
@@ -13,22 +96,22 @@ console.log(header)
 //     }
 
 /////// spot check 2//////////////
-$("h1").css("color", "blue")
-$(".red-div").css("color", "red")
-$("div li").css("color", "green")
+// $("h1").css("color", "blue")
+// $(".red-div").css("color", "red")
+// $("div li").css("color", "green")
+// // console.log(list)
+// // list[0]
+// // list[1].css("color", "pink")
+// const list = $("li")
 // console.log(list)
-// list[0]
-// list[1].css("color", "pink")
-const list = $("li")
-console.log(list)
-$("li:first-child").css("color", "green")
-$("li:last-child").css("color", "pink")
-$("#brown-div").css("color", "brown")
+// $("li:first-child").css("color", "green")
+// $("li:last-child").css("color", "pink")
+// $("#brown-div").css("color", "brown")
 
 /////// spot check 3//////////////
 
-$("#b1").addClass("box")
-$("#b2").addClass("box")
+// $("#b1").addClass("box")
+// $("#b2").addClass("box")
 
 // $('#my-input').val("v")
 
@@ -58,28 +141,89 @@ $("#b2").addClass("box")
 //     console.log($(this))
 // })
 
-$(".box").hover(function(){
-    $(this).css("background-color", "red")
-})
+// $(".box").hover(function(){
+//     $(this).css("background-color", "red")
+// })
 
-const aDynamicDiv = "<div class='mine'>Oh yes</div>"
-const elem = $(aDynamicDiv)
-console.log(elem)
-//////////////////////////
+// const aDynamicDiv = "<div class='mine'>Oh yes</div>"
+// const elem = $(aDynamicDiv)
+// console.log(elem)
+// //////////////////////////
 
-$('.fruits').append('<p class="red">Raspberry</p>')
-$('.fruits').append('<p class="brown">Kiwi</p>')
+// $('.fruits').append('<p class="red">Raspberry</p>')
+// $('.fruits').append('<p class="brown">Kiwi</p>')
 
-const text = "Banana"
-const item = $(`<p class=fruit>${text}ss</p>`)
+// const text = "Banana"
+// const item = $(`<p class=fruit>${text}ss</p>`)
 
-console.log(item) //the same
+// console.log(item) //the same
 /////////////spot check 9
 
-$(".feedme").on("click", function(){
+// $(".feedme").on("click", function(){
     
-    let divCopy = "<div class=feedme> ${$(this).text()} </div>"//use template literals and $(this)
-    // let divCopy = `<div class=feedme> ${$(this).text()} </div>`
+//     let divCopy = "<div class=feedme>${$(this).text()}</div>"//use template literals and $(this)
+//     // let divCopy = `<div class=feedme> ${$(this).text()} </div>`
 
-    return $("body").append(divCopy)
-  })
+//     return $("body").append(divCopy)
+//   })
+/////////////////spot check 10
+// const names = ["Alex", "Byron", "Cassandra", "Deandre", "Ellena"]
+
+// for(let name of names){
+//   $("body").append(`<div>${name}</div>`)
+// }
+
+// const names = [
+//     { first: "Alex", last: "Johnson" },
+//     { first: "Byron", last: "Loveall" },
+//     { first: "Cassandra", last: "Wuthers" },
+//     { first: "Deandre", last: "Rahm" },
+//     { first: "Ellena", last: "Freeman" }
+//   ]
+//<div class='human'>FIRST_NAME - LAST_NAME</div>  
+
+// for (let name of names){
+//     $("body").append(`<div class="human">${name.first} - ${name.last}</div>`)
+// }
+
+// $("p").hover(function(){
+//     $(this).remove()
+// })
+           
+// // let elem = "<div class=`blog`>some text</div>"
+// // let aDiv = $(elem)
+// $("#blogs").append( "<div class='blog'>Cool blog</div>" )
+// $("button").on("click", function(){
+//     $("#blogs").append("<div class='blog'>Cool blog</div>")
+//   })
+
+// $("button").on("click", function(){
+//     $(".blog").text("blargh")
+// })
+  
+//   $("#blogs").on("click", ".blog", function(){
+//     $(".blog").text("blargh") 
+//   })
+///////////////////////spot check 12
+
+//   const addDiv = function() {
+//     $("body").append("<div class=box></div>")
+//   };
+  
+//   $("button").on("click", function() {
+//     addDiv()
+//   });
+  
+//   $("body").on("click", ".box", function() {
+//     $(".box").text("new text")
+//   });
+
+
+//   $('#btn').alert('hi')
+// let posts = [{name: 'Shoobert'}, {name: 'Kayne'}]
+
+// for(let post of posts){
+//    console.log( $("#posts").append('<div>${post.name}</div><input type="text" placeholder="say something nice" />'))
+// }
+
+// posts.splice(0, 1)
